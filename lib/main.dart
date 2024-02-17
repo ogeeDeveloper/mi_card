69e72e5dc45b8b6197aa9dae90942a7c04d2d817
ogeeDeveloper <omarogrant202@hotmail.com>
2024-02-17 13:50:30 -0500

    Added the asset images folder to the pubsec file and used that image in the CircleAvatar widget

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('assets/images/Profile.jpeg'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
