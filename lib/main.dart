6c0a2575aff69d6fb159367ce6f6175735af2e41
ogeeDeveloper <omarogrant202@hotmail.com>
2024-02-17 15:10:36 -0500

    Added the Pacifico and SourceSans3 custom google fonts to the project

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,34 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50.0,
-                backgroundColor: Colors.red,
                 backgroundImage: AssetImage('assets/images/Profile.jpeg'),
+              ),
+              const Text(
+                'OgeeDeveloper',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Fullstack Developer'.toUpperCase(),
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontFamily: 'Source-Sans-3',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
               ),
             ],
           ),
